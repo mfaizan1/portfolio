2378349a90f7c53673c584e522ff5881feec5d13
faizan ul haq <mfaizanulhaq1@gmail.com>
2020-11-09 12:38:23 +0500

    init with navbar

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import React from "react"
+// import "../css/main.css"
+import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
+import Footer from "./Footer"
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <h2>layout component</h2>
+      {children}
+    </>
+  )
+}
+
+export default Layout
