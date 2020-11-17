1896ea7af992d0d0fe5b865e96006cf9a6fb000a
faizan ul haq <mfaizanulhaq1@gmail.com>
2020-11-17 13:42:04 +0500

    add contact us form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,41 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return (
+    <Layout>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>Get in touch</h3>
+          <form action="https://formspree.io/f/meqpzrnj" method="POST">
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Name"
+                className="form-control"
+                name="name"
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                className="form-control"
+                name="_replyto"
+              />
+              <textarea
+                name="message"
+                placeholder="Message"
+                className="form-control"
+                rows="5"
+                name="message"
+              ></textarea>
+            </div>
+            <button type="submit" className="submit-btn btn">
+              submit here
+            </button>
+          </form>
+        </article>
+      </section>
+    </Layout>
+  )
 }
 
 export default contact
