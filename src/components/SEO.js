099be4801b99292969a24a713873d7d822ff08cf
faizan ul haq <mfaizanulhaq1@gmail.com>
2020-11-17 19:04:28 +0500

    twitter card setup

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -32,6 +32,11 @@ const SEO = ({ description, title }) => {
     <Helmet title={`${title} | ${siteTitle}`} htmlAttributes={{ lang: "en" }}>
       <meta name="description" content={description || siteDescription} />
       <meta name="image" content={image} />
+      <meta name="twitter:card" content="summary_large_iamge" />
+      <meta name="twitter:creator" content={twitterUsername} />
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={siteDescription} />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
     </Helmet>
   )
 }
